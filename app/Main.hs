c26a74cf84c08cc3233a5e004b0bb769b4901301
amderbar <amderbar@gmail.com>
2021-11-20 14:16:51 +0900

    feat: RSS をダウンロードしてパースしてターミナルに表示する

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Control.Lens ((^.))
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as Txt
+import qualified Network.Wreq as Wreq
+import System.Environment (getArgs)
+import qualified Text.Feed.Export as Export (textFeedWith)
+import qualified Text.Feed.Import as Import (parseFeedSource)
+import Text.Feed.Types (Feed)
+import Text.XML (def, rsPretty)
 
 main :: IO ()
-main = someFunc
+main = do
+  [uri] <- getArgs
+  res <- Wreq.get uri
+  mapM_ Txt.putStr $ renderFeed =<< Import.parseFeedSource (res ^. Wreq.responseBody)
+
+renderFeed :: Feed -> Maybe Text
+renderFeed = Export.textFeedWith def {rsPretty = True}
