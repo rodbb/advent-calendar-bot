eada75b527a080571018776762a1787c7a61e9bf
amderbar <amderbar@gmail.com>
2021-11-20 17:06:44 +0900

    feat: 記事タイトルのリストとしてターミナルに表示する

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,32 @@
 module Main where
 
 import Control.Lens ((^.))
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy.IO as Txt
+import Data.Text (Text)
+import qualified Data.Text as Txt
+import qualified Data.Text.IO as Txt
 import qualified Network.Wreq as Wreq
+import qualified Network.Mattermost.Endpoints as Mttmst
 import System.Environment (getArgs)
-import qualified Text.Feed.Export as Export (textFeedWith)
+import qualified Text.Atom.Feed as Atom
 import qualified Text.Feed.Import as Import (parseFeedSource)
-import Text.Feed.Types (Feed)
-import Text.XML (def, rsPretty)
+import Text.Feed.Types (Feed (AtomFeed))
 
 main :: IO ()
 main = do
   [uri] <- getArgs
   res <- Wreq.get uri
-  mapM_ Txt.putStr $ renderFeed =<< Import.parseFeedSource (res ^. Wreq.responseBody)
+  mapM_ Txt.putStr $ renderFeed <$> Import.parseFeedSource (res ^. Wreq.responseBody)
 
-renderFeed :: Feed -> Maybe Text
-renderFeed = Export.textFeedWith def {rsPretty = True}
+renderFeed :: Feed -> Text
+renderFeed feed = case feed of
+  AtomFeed f -> renderAsMarkdown $ getEntryTitleAsText <$> Atom.feedEntries f
+  _ -> Txt.empty
+  where
+    getEntryTitleAsText :: Atom.Entry -> Text
+    getEntryTitleAsText entry = case Atom.entryTitle entry of
+      Atom.TextString t -> t
+      Atom.HTMLString t -> t
+      t@(Atom.XHTMLString _) -> Txt.pack (Atom.txtToString t)
+
+    renderAsMarkdown :: [Text] -> Text
+    renderAsMarkdown = Txt.unlines . fmap (Txt.append "* ")
