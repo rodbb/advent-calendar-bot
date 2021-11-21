9f1ed39629d301e303309eff617947a0b44297ce
amderbar <amderbar@gmail.com>
2021-11-21 10:59:31 +0900

    feat: Mattermost に投稿する

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
 import Control.Lens ((^.))
+import Control.Monad (void)
 import Data.Text (Text)
 import qualified Data.Text as Txt
-import qualified Data.Text.IO as Txt
+import qualified Network.Mattermost as Mttrmst
+  ( defaultConnectionPoolConfig,
+    initConnectionData,
+  )
+import qualified Network.Mattermost.Endpoints as Mttrmst
+  ( mmCreatePost,
+  )
+import qualified Network.Mattermost.Types as Mttrmst
+  ( ChannelId (CI),
+    ConnectionType (ConnectHTTP),
+    Hostname,
+    Id (Id),
+    Port,
+    Session (Session, sessConn, sessTok),
+    rawPost,
+  )
+import qualified Network.Mattermost.Types.Internal as Mttrmst
+  ( Token (Token),
+  )
 import qualified Network.Wreq as Wreq
-import qualified Network.Mattermost.Endpoints as Mttmst
 import System.Environment (getArgs)
 import qualified Text.Atom.Feed as Atom
 import qualified Text.Feed.Import as Import (parseFeedSource)
@@ -15,9 +34,13 @@ import Text.Feed.Types (Feed (AtomFeed))
 
 main :: IO ()
 main = do
-  [uri] <- getArgs
-  res <- Wreq.get uri
-  mapM_ Txt.putStr $ renderFeed <$> Import.parseFeedSource (res ^. Wreq.responseBody)
+  [targetHost, targetPort, mttrParsonalAccessToken, taegetChannelId, feedUri] <- getArgs
+  let mttrHost = Txt.pack targetHost
+  let mttrPort = read targetPort
+  let chanId = Txt.pack taegetChannelId
+  res <- Wreq.get feedUri
+  let theMsg = renderFeed <$> Import.parseFeedSource (res ^. Wreq.responseBody)
+  mapM_ (postToMattermost MttrInfo {..} chanId) theMsg
 
 renderFeed :: Feed -> Text
 renderFeed feed = case feed of
@@ -32,3 +55,28 @@ renderFeed feed = case feed of
 
     renderAsMarkdown :: [Text] -> Text
     renderAsMarkdown = Txt.unlines . fmap (Txt.append "* ")
+
+type ParsonalAccessToken = String
+
+data MttrInfo = MttrInfo
+  { mttrHost :: Mttrmst.Hostname,
+    mttrPort :: Mttrmst.Port,
+    mttrParsonalAccessToken :: ParsonalAccessToken
+  }
+
+type ChannelId = Text
+
+postToMattermost :: MttrInfo -> ChannelId -> Text -> IO ()
+postToMattermost MttrInfo {..} channelId theMsg =
+  let sessTok = Mttrmst.Token mttrParsonalAccessToken
+      chanId = Mttrmst.CI (Mttrmst.Id channelId)
+      postObj = Mttrmst.rawPost theMsg chanId
+   in do
+        sessConn <-
+          Mttrmst.initConnectionData
+            mttrHost
+            mttrPort
+            Txt.empty
+            Mttrmst.ConnectHTTP
+            Mttrmst.defaultConnectionPoolConfig
+        void $ Mttrmst.mmCreatePost postObj Mttrmst.Session {..}
