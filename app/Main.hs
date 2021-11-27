e223c86a61a871679edf72dc0b61f4975f04b43c
amderbar <amderbar@gmail.com>
2021-11-27 10:11:48 +0900

    feat: テンプレート用Viewモデル作成

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -16,10 +17,9 @@ import Data.Aeson
     toJSON,
   )
 import Data.ByteString.Lazy (ByteString)
-import Data.Function ((&))
-import Data.Functor ((<&>))
+import Data.List (find, uncons)
 import Data.Text (Text)
-import qualified Data.Text as Txt (append, pack, unlines, unpack)
+import qualified Data.Text as Txt (pack, unpack)
 import qualified Data.Text.IO as Txt (readFile, writeFile)
 import Data.Time
   ( ParseTime,
@@ -47,6 +47,8 @@ import qualified Text.Atom.Feed as Atom
 import qualified Text.Feed.Import as Import (parseFeedSource)
 import qualified Text.Feed.Query as Feed
 import Text.Feed.Types (Feed (AtomFeed))
+import Text.URI (mkURI)
+import qualified Text.URI as URI (relativeTo, render)
 
 data Args = Args
   { mttrWebhookUrl :: String,
@@ -99,33 +101,85 @@ getCalendarFeed feedUri = do
 parseFeedDate :: (MonadFail m, ParseTime t) => Text -> m t
 parseFeedDate = parseTimeM True defaultTimeLocale "%FT%T%Ez" . Txt.unpack
 
+data AdventCalendar = AdventCalendar
+  { _calendarTitle :: Text,
+    _calendarUrl :: Text,
+    _calendarUpdate :: Text,
+    _calendarEntries :: [CalendarEntry]
+  }
+  deriving (Show)
+
+data CalendarEntry = CalendarEntry
+  { _entryTitle :: Text,
+    _entryAuther :: Text,
+    _entryUrl :: Text,
+    _entrySummary :: Maybe Text,
+    _entryPublished :: UTCTime
+  }
+  deriving (Show)
+
 renderFeed :: Maybe UTCTime -> Feed -> Maybe Text
 renderFeed mTime feed = case feed of
-  AtomFeed f ->
-    Atom.feedEntries f
-      & filter isNewEntry
-      <&> getEntryTitleAsText
-      & renderAsMarkdown
-      & Just
+  AtomFeed f -> renderAsMarkdown . pickupNewEntryAfter mTime <$> fromAtomFeed f
   _ -> Nothing
   where
-    isNewEntry :: Atom.Entry -> Bool
-    isNewEntry entry =
-      let pubDate =
-            (parseFeedDate =<< Atom.entryPublished entry)
-              <|> parseFeedDate (Atom.entryUpdated entry)
-       in case mTime of
-            Nothing -> True
-            Just _ -> pubDate > mTime
+    fromAtomFeed :: Atom.Feed -> Maybe AdventCalendar
+    fromAtomFeed f = do
+      _calendarUrl <- calendarUrl f
+      _calendarEntries <- traverse fromAtomEntry (Atom.feedEntries f)
+      return
+        AdventCalendar
+          { _calendarTitle = getContentAsText (Atom.feedTitle f),
+            _calendarUrl,
+            _calendarUpdate = Atom.feedUpdated f,
+            _calendarEntries
+          }
 
-    getEntryTitleAsText :: Atom.Entry -> Text
-    getEntryTitleAsText entry = case Atom.entryTitle entry of
+    calendarUrl :: Atom.Feed -> Maybe Text
+    calendarUrl f = do
+      selfLink <- findSelf (Atom.feedLinks f)
+      selfUri <- URI.relativeTo <$> mkURI "." <*> mkURI (Atom.linkHref selfLink)
+      URI.render <$> selfUri
+      where
+        findSelf = find (\Atom.Link {linkRel} -> linkRel == Just (Right "self"))
+
+    fromAtomEntry :: Atom.Entry -> Maybe CalendarEntry
+    fromAtomEntry e = do
+      (_entryAuther, _) <- uncons $ Atom.personName <$> Atom.entryAuthors e
+      _entryUrl <- entryUrl e
+      _entryPublished <- (parseFeedDate =<< Atom.entryPublished e) <|> parseFeedDate (Atom.entryUpdated e)
+      return
+        CalendarEntry
+          { _entryTitle = getContentAsText (Atom.entryTitle e),
+            _entryAuther,
+            _entryUrl,
+            _entrySummary = Nothing,
+            _entryPublished
+          }
+
+    entryUrl :: Atom.Entry -> Maybe Text
+    entryUrl e = Atom.linkHref <$> findAlternate (Atom.entryLinks e)
+      where
+        findAlternate = find (\Atom.Link {linkRel} -> linkRel == Just (Right "alternate"))
+
+    pickupNewEntryAfter :: Maybe UTCTime -> AdventCalendar -> AdventCalendar
+    pickupNewEntryAfter mt ac@AdventCalendar {_calendarEntries} =
+      ac {_calendarEntries = filter (isNewEntry mt) _calendarEntries}
+
+    isNewEntry :: Maybe UTCTime -> CalendarEntry -> Bool
+    isNewEntry mt CalendarEntry {_entryPublished} =
+      case mt of
+        Nothing -> True
+        Just t -> _entryPublished > t
+
+    getContentAsText :: Atom.TextContent -> Text
+    getContentAsText cntnt = case cntnt of
       Atom.TextString t -> t
       Atom.HTMLString t -> t
       t@(Atom.XHTMLString _) -> Txt.pack (Atom.txtToString t)
 
-    renderAsMarkdown :: [Text] -> Text
-    renderAsMarkdown = Txt.unlines . fmap (Txt.append "* ")
+    renderAsMarkdown :: AdventCalendar -> Text
+    renderAsMarkdown = Txt.pack . show
 
 type WebhookUrl = String
 
